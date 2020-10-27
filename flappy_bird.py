975fa190f42acc01b5348f3d45b34131d22305ca
Guillermo Lerio Arroyo <guillermo.leiro.arroyo@ibm.com>
2020-10-27 22:23:04 +0100

    making bg fullsize

--- flappy_bird.py
+++ flappy_bird.py
@@ -5,7 +5,8 @@ pygame.init()
 screen = pygame.display.set_mode((576,1024))
 clock = pygame.time.Clock()
 
-bg_surface = pygame.image.load('sprites/background-day.png')
+bg_surface = pygame.image.load('sprites/background-day.png').convert()
+bg_surface = pygame.transform.scale2x(bg_surface)
 
 while True:
     for event in pygame.event.get():
