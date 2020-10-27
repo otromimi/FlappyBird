2d0ee4a996bae02b0ebce256817bc57ed98fe3cc
Guillermo Lerio Arroyo <guillermo.leiro.arroyo@ibm.com>
2020-10-27 23:13:00 +0100

    collision logic

--- flappy_bird.py
+++ flappy_bird.py
@@ -24,7 +24,16 @@ def draw_pipes(pipes):
         else:
             flip_pipe = pygame.transform.flip(pipe_surface, False, True)
             screen.blit(flip_pipe, pipe)
-            
+
+def check_collision(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            print('collision')
+    
+    if bird_rect.top <= -100 or bird_rect.bottom >= 900:
+        print('collision')
+
+
 pygame.init()
 screen = pygame.display.set_mode((576,1024))
 clock = pygame.time.Clock()
@@ -70,6 +79,7 @@ while True:
     bird_movement += gravity
     bird_rect.centery += bird_movement
     screen.blit(bird_surface, bird_rect)
+    check_collision(pipe_list)
 
     # Pipes
     pipe_list = move_pipes(pipe_list)
