b7232a451ef1a9da85eadbced76f3c61040a914a
Guillermo Lerio Arroyo <guillermo.leiro.arroyo@ibm.com>
2020-10-27 22:19:51 +0100

    loading background

--- flappy_bird.py
+++ flappy_bird.py
@@ -5,11 +5,15 @@ pygame.init()
 screen = pygame.display.set_mode((576,1024))
 clock = pygame.time.Clock()
 
+bg_surface = pygame.image.load('sprites/background-day.png')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    screen.blit(bg_surface,(0,0))
 
     pygame.display.update() 
     clock.tick(120)
