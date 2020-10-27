2e41d1afe4d7f3cfa30f65c7412b174f0b31af40
Guillermo Lerio Arroyo <guillermo.leiro.arroyo@ibm.com>
2020-10-27 22:26:06 +0100

    adding floor

--- flappy_bird.py
+++ flappy_bird.py
@@ -8,6 +8,10 @@ clock = pygame.time.Clock()
 bg_surface = pygame.image.load('sprites/background-day.png').convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
 
+floor_surface = pygame.image.load('sprites/base.png').convert()
+floor_surface = pygame.transform.scale2x(floor_surface)
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -15,6 +19,7 @@ while True:
             sys.exit()
 
     screen.blit(bg_surface,(0,0))
+    screen.blit(floor_surface,(0,900))
 
     pygame.display.update() 
     clock.tick(120)
