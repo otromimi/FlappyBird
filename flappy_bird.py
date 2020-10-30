bd5501c2d8ac783a6b5712a3cc56b6a15d09575c
Guillermo Lerio Arroyo <guillermo.leiro.arroyo@ibm.com>
2020-10-30 02:00:55 +0100

    scores working

--- flappy_bird.py
+++ flappy_bird.py
@@ -45,14 +45,17 @@ def bird_animation():
     return new_bird, new_bird_rect
 
 def score_display(game_state):
-    #if game_state == 'main_game':
-    score_surface = game_font.render(str(int(score)), True, (255, 255 ,255))
-    score_rect = score_surface.get_rect(center = (288, 100))
-    screen.blit(score_surface, score_rect)
+    score_string  = ""
+    if game_state == 'main_game':
+        score_string = str(int(score))
     if game_state == 'game_over':
-        high_score_surface = game_font.render(str(int(high_score)), True, (255, 255 ,255))
+        score_string = f'Score: {str(int(score))}'
+        high_score_surface = game_font.render(f'High_Score: {str(int(high_score))}', True, (255, 255 ,255))
         high_score_rect = high_score_surface.get_rect(center = (288, 850))
         screen.blit(high_score_surface, high_score_rect)
+    score_surface = game_font.render(score_string, True, (255, 255 ,255))
+    score_rect = score_surface.get_rect(center = (288, 100))
+    screen.blit(score_surface, score_rect)
 
 
 pygame.init()
@@ -113,6 +116,7 @@ while True:
                 pipe_list.clear()
                 bird_rect.center = (100, 512)
                 bird_movement = 0
+                score = 0
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipe())
         if event.type == BIRDFLAP:
@@ -138,6 +142,8 @@ while True:
         score_display("main_game")
     else:
         score_display("game_over")
+        if high_score < score:
+            high_score = score
 
     # Floor
     floor_x_pos -= 1
