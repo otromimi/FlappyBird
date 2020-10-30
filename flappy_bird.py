3d4fb0f8e6ae2a103f05e9814c94476d59e3c97d
Guillermo Lerio Arroyo <guillermo.leiro.arroyo@ibm.com>
2020-10-30 02:08:22 +0100

    game over screen

--- flappy_bird.py
+++ flappy_bird.py
@@ -101,6 +101,10 @@ SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1200)
 pipe_height = [400,600,800]
 
+# Game over looks
+game_over_surface = pygame.transform.scale2x(pygame.image.load('sprites/message.png').convert_alpha())
+game_over_rect = game_over_surface.get_rect(center = (288,512))
+
 
 while True:
     for event in pygame.event.get():
@@ -141,9 +145,13 @@ while True:
         score += 0.01
         score_display("main_game")
     else:
+        screen.blit(game_over_surface, game_over_rect)
         score_display("game_over")
+
+        # Score updates
         if high_score < score:
             high_score = score
+        
 
     # Floor
     floor_x_pos -= 1
