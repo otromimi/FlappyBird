a611aed6e0eb71d6e10717ff1fde934771bfd584
Guillermo Lerio Arroyo <guillermo.leiro.arroyo@ibm.com>
2020-10-30 01:51:26 +0100

    showing best score but a little fuck up

--- flappy_bird.py
+++ flappy_bird.py
@@ -44,10 +44,16 @@ def bird_animation():
     new_bird_rect = new_bird.get_rect(center = (100, bird_rect.centery))
     return new_bird, new_bird_rect
 
-def score_display():
+def score_display(game_state):
+    #if game_state == 'main_game':
     score_surface = game_font.render(str(int(score)), True, (255, 255 ,255))
     score_rect = score_surface.get_rect(center = (288, 100))
     screen.blit(score_surface, score_rect)
+    if game_state == 'game_over':
+        high_score_surface = game_font.render(str(int(high_score)), True, (255, 255 ,255))
+        high_score_rect = high_score_surface.get_rect(center = (288, 850))
+        screen.blit(high_score_surface, high_score_rect)
+
 
 pygame.init()
 screen = pygame.display.set_mode((576,1024))
@@ -58,7 +64,7 @@ game_font = pygame.font.Font('04B_19.ttf', 40)
 gravity = 0.25
 bird_movement = 0
 game_active = True
-score , height_score = 0, 0
+score , high_score = 0, 0
 
 bg_surface = pygame.image.load('sprites/background-day.png').convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
@@ -129,7 +135,9 @@ while True:
 
         # Score
         score += 0.01
-        score_display()
+        score_display("main_game")
+    else:
+        score_display("game_over")
 
     # Floor
     floor_x_pos -= 1
